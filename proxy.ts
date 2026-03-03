80f08eb3d1f6e66eb6a48cfdf20af907c1b6becd
mariana <maranamelen13@gmail.com>
2026-03-03 19:28:07 +0200

    fix proxy

--- proxy.ts
+++ proxy.ts
@@ -3,7 +3,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
 import { parse } from 'cookie';
-import { checkServerSession } from './lib/api/serverApi';
+import { checkSession } from './lib/api/serverApi';
 
 const privateRoutes = ['/profile', '/notes'];
 const publicRoutes = ['/sign-in', '/sign-up'];
@@ -23,7 +23,7 @@ export async function proxy(request: NextRequest) {
         if (refreshToken) {
             // Якщо accessToken відсутній, але є refreshToken — потрібно перевірити сесію навіть для публічного маршруту,
             // адже сесія може залишатися активною, і тоді потрібно заборонити доступ до публічного маршруту.
-            const data = await checkServerSession();
+            const data = await checkSession();
             const setCookie = data.headers['set-cookie'];
 
             if (setCookie) {
